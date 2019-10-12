32ed2228f1a4ca434c9ad22a2b2853d279531f9c
@Winternight5 <tai.p.huynh@gmail.com>
2019-10-12 04:18:05 -0700

    updated db config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
-
+	
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
