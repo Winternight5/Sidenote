e5607876807329c0c80e5128c23dae171038cc0c
William Nguyen <williamnguyen@Williams-MacBook-Pro.local>
2019-10-30 20:31:33 -0700

    some css

--- app/static/welcomeFadeInAnimation.js
+++ app/static/welcomeFadeInAnimation.js
@@ -3,25 +3,24 @@
   the login/registration form.
 */
 
-$(document).ready(function()
-{
+$(document).ready(function () {
 
-const delayTimer = 1;
+  const delayTimer = 1;
 
-var $header = $("h1");
-var $aboutSidenote = $(".introduction p");
-var $loginForm = $("form");
-var $learnMoreButton = $(".ghost-button button");
+  var $header = $("h4");
+  var $aboutSidenote = $("img");
+  var $loginForm = $("form");
+  var $learnMoreButton = $("button");
 
-$header.hide();
-$aboutSidenote.hide();
-$loginForm.hide();
-$learnMoreButton.hide();
+  $header.hide();
+  $aboutSidenote.hide();
+  $loginForm.hide();
+  $learnMoreButton.hide();
 
-$header.delay(50 * delayTimer * 3).fadeIn(1450);
-$aboutSidenote.delay(50 * delayTimer * 10).fadeIn(1450);
-$learnMoreButton.delay(50 * delayTimer * 25).fadeIn(1450);
-$loginForm.delay(50 * delayTimer * 25).fadeIn(1450); 
+  $header.delay(50 * delayTimer * 3).fadeIn(1450);
+  $aboutSidenote.delay(50 * delayTimer * 10).fadeIn(1450);
+  $learnMoreButton.delay(50 * delayTimer * 25).fadeIn(1450);
+  $loginForm.delay(50 * delayTimer * 25).fadeIn(1450);
 
 
 });
