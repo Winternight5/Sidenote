1260e60c9ed3c786adab432920945660556d92c0
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2019-11-09 04:29:37 -0800

    script now fades in the welcome header text

---
+++ Team4/app/static/welcomeFadeInAnimation.js
@@ -0,0 +1,26 @@
+/*
+  This script utilizes JavaScript and jQuery to fade in the Welcome Page's contents, including the header3 and
+  the login/registration form.
+*/
+
+$(document).ready(function () {
+
+    const delayTimer = 1;
+  
+    var $header = $("h3");
+    var $aboutSidenote = $("img");
+    var $loginForm = $("form");
+    var $learnMoreButton = $("button");
+  
+    $header.hide();
+    $aboutSidenote.hide();
+    $loginForm.hide();
+    $learnMoreButton.hide();
+  
+    $header.delay(50 * delayTimer * 3).fadeIn(1450);
+    $aboutSidenote.delay(50 * delayTimer * 10).fadeIn(1450);
+    $learnMoreButton.delay(50 * delayTimer * 25).fadeIn(1450);
+    $loginForm.delay(50 * delayTimer * 25).fadeIn(1450);
+  
+  
+  });
