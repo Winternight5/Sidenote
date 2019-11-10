1c60b34a35287a4fc3a63c3e32d2858f580ad624
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2019-11-09 23:30:05 -0800

    code clean up

--- app/static/welcomeFadeInAnimation.js
+++ app/static/welcomeFadeInAnimation.js
@@ -5,23 +5,23 @@
 
 $(document).ready(function() {
 
-  const delayTimer = 1;
-
-  // fixed?
+  // retrieving the html elements
   var $header = $("h3");
   var $aboutSidenote = $("img");
   var $loginForm = $("form");
   var $learnMoreButton = $("button");
 
+  // hiding the elements
   $header.hide();
   $aboutSidenote.hide();
   $loginForm.hide();
   $learnMoreButton.hide();
 
-  $header.delay(50 * delayTimer * 3).fadeIn(1450);
-  $aboutSidenote.delay(50 * delayTimer * 10).fadeIn(1450);
-  $learnMoreButton.delay(50 * delayTimer * 25).fadeIn(1450);
-  $loginForm.delay(50 * delayTimer * 25).fadeIn(1450);
+  // delaying and fading in the elements
+  $header.delay(50 * 3).fadeIn(1450);
+  $aboutSidenote.delay(50 * 10).fadeIn(1450);
+  $learnMoreButton.delay(50 * 25).fadeIn(1450);
+  $loginForm.delay(50 * 25).fadeIn(1450);
 
 
 });
