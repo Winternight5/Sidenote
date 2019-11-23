68e8ca99c8abc409a7d8524d5ce5f91dee71cc3e
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2019-11-22 23:19:56 -0800

    nav links now fade in

--- app/static/welcomeFadeInAnimation.js
+++ app/static/welcomeFadeInAnimation.js
@@ -5,24 +5,35 @@
 
 $(document).ready(function() {
 
+  var navLinkCount = 4;
   // retrieving the html elements
-  var $header = $("h3");
+  var $navLinks = $("nav a");
+  var $welcome = $("h3");
   var $aboutSidenote = $("img");
   var $loginForm = $("form");
   var $learnMoreButton = $("button");
 
   // hiding the elements
-  $header.hide();
+  $navLinks.hide();
+  $welcome.hide();
   $aboutSidenote.hide();
   $loginForm.hide();
   $learnMoreButton.hide();
 
   // delaying and fading in the elements
-  $header.delay(50 * 3).fadeIn(1450);
+  fadeLinks($navLinks);
+  $welcome.delay(50 * 3).fadeIn(1450);
   $aboutSidenote.delay(50 * 10).fadeIn(1450);
   $learnMoreButton.delay(50 * 25).fadeIn(1450);
   $loginForm.delay(50 * 25).fadeIn(1450);
 
+  function fadeLinks($navLinks)
+  {
+    $navLinks.each(function(index)
+    {
+      $(this).delay(50 * index).fadeIn(1000);
+    });
+  }
 
 });
 
